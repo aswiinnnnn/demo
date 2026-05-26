af6af67ee0728798244c18cc7171cf3a572366d2
aswiinnnnn <ashwinraj9072a@gmail.com>
2026-05-26 23:16:30 +0530

    feat: add product list and create endpoint URL configuration

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,5 @@ from .views import product_list_create
 
 
 urlpatterns = [
-    path("products/", product_list_create, name="product-lidasdast-create"),
+    path("products/", product_list_create, name="product-lidasasjdjwddast-create"),
 ]
